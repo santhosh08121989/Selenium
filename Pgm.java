dbd5400494814514740ebfc7e4dc96c34a7046b8
santhosh08121989 <61314603+santhosh08121989@users.noreply.github.com>
2020-03-13 15:05:47 +0530

    Add files via upload

---
+++ Pgm.java
@@ -0,0 +1,22 @@
+class Pgm 
+{
+Pgm()
+{
+System.out.println("Welcome to DevOps")
+}
+Pgm(int a,int b)
+{
+System.out.print(a+" "+b);
+}
+Pgm(int a,int b,int c)
+{
+System.out.print(a+" "+b+" "+c)
+}
+public static void main(String args[])
+{
+Pgm p=new Pgm();
+Pgm p=new Pgm(10,20);
+Pgm p=new Pgm(10,20,30);
+}
+}
+`
